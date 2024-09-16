9401debbbf6c77d9bd01b53db51aba931f14b397
Harri Rekola <harri.rekola@tuni.fi>
2024-09-16 18:16:17 +0300

    first commit

---
+++ qrcodegen.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+from PIL import ImageTk
+import qrcode
+import time
+from datetime import datetime, timedelta
+from decimal import Decimal
+
+def generate_qr_code(data):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=15,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    # Convert QR code to PIL image
+    img = qr.make_image(fill='black', back_color='white').convert("RGB")
+    return img
+
+def start_qr_code_generation():
+    start_button.pack_forget()
+    root.after(5000, lambda: update_qr_code(0))
+
+def update_qr_code(counter):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+
+    qr_data = f"Frame number {counter} Timestamp: {timestamp}"
+
+    start = time.perf_counter()
+    img = generate_qr_code(qr_data)
+    end = time.perf_counter()
+    elapsed_time = end - start
+
+    tk_img = ImageTk.PhotoImage(img)
+    qr_label.config(image=tk_img)
+    qr_label.image = tk_img
+
+    timestamp_dt = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
+    adjusted_timestamp_dt = timestamp_dt - timedelta(seconds=elapsed_time)
+    adjusted_timestamp = adjusted_timestamp_dt.strftime("%Y-%m-%d %H:%M:%S.%f")
+
+    timestamp_label.config(text=f"Original Timestamp: {timestamp} | Adjusted Timestamp: {adjusted_timestamp} | Processing Time: {elapsed_time:.6f} seconds")
+
+    # 1000 ms / desired FPS
+    root.after(int(Decimal(1000) / Decimal(60)), update_qr_code, counter + 1)
+
+if __name__ == "__main__":
+    # tkinter root window
+    root = tk.Tk()
+    root.geometry("300x300")
+    root.attributes('-fullscreen', True)
+    root.configure(background='white')
+
+    # display QR code
+    qr_label = tk.Label(root)
+    qr_label.pack()
+
+    # display timestamp
+    timestamp_label = tk.Label(root, font=("Arial", 14), bg='black')
+    timestamp_label.pack()
+
+    # button to start the QR code generation
+    start_button = tk.Button(root, text="Start", font=("Arial", 14), command=start_qr_code_generation)
+    start_button.pack(pady=20)
+
+    root.mainloop()
